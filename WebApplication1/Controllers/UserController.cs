f2354138c7683a3c31d7926cf4913214c4a560cf
Jeikhun Jalilov <jeykhunvj@code.edu.az>
2022-10-05 15:30:09 +0400

    added

---
+++ WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class UserController:Controller
+    {
+        public ViewResult Index()
+        {
+            ViewBag.Name = "Jeikhun";
+            ViewBag.Surname = "Jalilov";
+            ViewBag.Age = "21";
+            ViewBag.Country = "Georgia";
+            ViewBag.PhoneNumber = "+994708978997";
+
+            return View();
+
+        }
+    }
+}
